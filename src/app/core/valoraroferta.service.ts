12407e69de512e3a82163c678857ceccb8dafa4e
DIGITEACHBOX29041209 <lina.fernanda.rc@gmail.com>
2022-08-16 15:00:43 -0500

    avances

--- src/app/core/valoraroferta.service.ts
+++ src/app/core/valoraroferta.service.ts
@@ -26,4 +26,24 @@ export class ValorarofertaService {
   ConsultaOferta(Bandera: string, idOferta: string) {
     return this.http.get<any[]>(this.url_servidor + 'conscoferta/' + Bandera + '/' + idOferta)
   }
+
+  ConsultaSectores(Bandera:string, NomSector:string, CdPais:String, Cd_Region:string, Cd_Mncpio:string){
+    return this.http.get<any[]>(this.url_servidor+'consectores/'+Bandera+'/'+NomSector+'/'+CdPais+'/'+Cd_Region+'/'+Cd_Mncpio)
+  }
+  
+  OperacionSectores(bandera:string, Body:any){
+    return this.http.post<any>(this.url_servidor+'csectorofertamod/'+bandera, Body)
+  }
+
+  ConsultaSectoresOferta(bandera:string, CD_CNSCTVO:string){
+    return this.http.get<any[]>(this.url_servidor+'conscsectoroferta/'+bandera+'/'+CD_CNSCTVO)
+  }
+
+  InsertarSector(bandera: string, Body: any){
+    return this.http.post<any>(this.url_servidor+'sectoresmod/'+bandera, Body)
+  }
+
+  ConsultaCiudadOferta(bandera:string, CD_CNSCTVO:string){
+    return this.http.get<any[]>(this.url_servidor+'conscciudadistcionofert/'+bandera+'/'+CD_CNSCTVO)
+  }
 }
--- src/app/ofertas/components/valoraroferta/sectorizacion/sectorizacion.component.ts
+++ src/app/ofertas/components/valoraroferta/sectorizacion/sectorizacion.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ValorarofertaService } from 'src/app/core/valoraroferta.service';
 
 @Component({
   selector: 'app-sectorizacion',
@@ -9,42 +10,131 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class SectorizacionComponent implements OnInit {
   DesSect: string = '';
   CoordeSect: string = '';
-  Cant: string ='';
-  VlrFle: string ='';
+  Cant: string = '';
+  VlrFle: string = '';
+  DataSectores: any;
+  keyword: string = '';
+  SectSelec: any;
+  Respuesta: any;
+  SessionOferta: any;
+  DataSectorOferta: any[] = [];
+  ValidaConsulta: string = '';
+  txtValidaCons: string = '';
+  NombreSec: string = '';
+  DescSec: string = '';
+  Coor1: string = '';
+  Coor2: string = '';
+  SessionCDMunicipio: any;
+  SessionCDRegion: any;
+  SessionCiudad: any;
+  SessionIdUsuario: any;
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, public sectoresservices: ValorarofertaService) { }
 
   ngOnInit(): void {
     this.DesSect = '';
-    this.CoordeSect='';
-    this.Cant='';
-    this.VlrFle='';
+    this.CoordeSect = '';
+    this.Cant = '';
+    this.VlrFle = '';
+    this.SectSelec = '';
+    this.SessionOferta = '1011'
+    this.SessionIdUsuario = '1'
+    this.ConsultaSectores();
+    this.ConsultaCiudadOferta();
+    this.ConsultaSectoresOferta();
+
+  }
+  ConsultaCiudadOferta() {
+    this.sectoresservices.ConsultaCiudadOferta('1',this.SessionOferta).subscribe(ResultadoCons=>{
+      this.SessionCiudad=ResultadoCons[0].Cuidad;  
+      this.SessionCDMunicipio=ResultadoCons[0].CD_MNCPIO;
+      this.SessionCDRegion=ResultadoCons[0].CD_RGION;      
+    })
+  }
+  ConsultaSectoresOferta() {
+    this.sectoresservices.ConsultaSectoresOferta('1', this.SessionOferta).subscribe(ResultConsulta => {
+      if (ResultConsulta.length > 0) {
+        this.ValidaConsulta = '0';
+        this.DataSectorOferta = ResultConsulta;
+      }
+      else {
+        this.ValidaConsulta = '1';
+        this.DataSectorOferta = [];
+        this.txtValidaCons = 'No se encuentran registros de sectores asociados a la oferta';
+      }
+    })
+  }
+
+  ConsultaSectores() {
+    this.sectoresservices.ConsultaSectores('1', '0', '0', '0', '0').subscribe(Result => {
+      this.DataSectores = Result;
+      this.keyword = 'DSCRPCION_SCTOR';
+    })
+  }
+
+  AsociaSector(templateRespuesta: any) {
+    this.Respuesta = '';
+    if (this.Cant != '' && this.VlrFle != '' && this.SectSelec != '') {
+      const BodyInsert = {
+        ID: "0",
+        CD_CNSCTVO: this.SessionOferta,
+        ID_SCTOR_OFRTA: this.SectSelec,
+        CNTDAD: this.Cant,
+        VLOR_FLTE_SGRDO: this.VlrFle
+      }
+      console.log(BodyInsert)
+      this.sectoresservices.OperacionSectores('3', BodyInsert).subscribe(ResultInsert => {
+        var respuesta = ResultInsert.split('|')
+        this.modalService.open(templateRespuesta, { ariaLabelledBy: 'modal-basic-title' })
+        this.Respuesta = respuesta[1]
+        this.ConsultaSectoresOferta();
+      })
+    }
+    else {
+      this.modalService.open(templateRespuesta, { ariaLabelledBy: 'modal-basic-title' })
+      this.Respuesta = 'Los campos Sector, Cantidad y Valor flete son obligatorios, favor valida tu información.';
+    }
 
   }
 
-  keyword = 'name';
-  data = [
-    {
-      id: 1,
-      name: 'Georgia'
-    },  
-    {
-      id: 2,
-      name: 'Usa'
-    },
-    {
-      id: 3,
-      name: 'England'
+  EliminaSector(sector: any, templateRespuesta: any) {
+    const BodyDelete = {
+      ID: "0",
+      CD_CNSCTVO: sector.CD_CNSCTVO,
+      ID_SCTOR_OFRTA: sector.ID_SCTOR_OFRTA,
+      CNTDAD: sector.CNTDAD,
+      VLOR_FLTE_SGRDO: sector.VLOR_FLTE_SGRDO
     }
-  ];
+    this.sectoresservices.OperacionSectores('4', BodyDelete).subscribe(ResultDelet => {
+      var respuesta = ResultDelet.split('|')
+      this.modalService.open(templateRespuesta, { ariaLabelledBy: 'modal-basic-title' })
+      this.Respuesta = respuesta[1]
+      this.ConsultaSectoresOferta();
+    })
+  }
 
-
-  open(content: any) {
+  CreaSector(content: any) {
     this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: 'lg' })
+    const BodyInsert={
+      USUCODIG: this.SessionIdUsuario,
+      SCTOR_OFR: 0,
+      DSCRPCION_SCTOR: "PRUEBA JJ",
+      LAT_NORTE: "4.7009775788652295",
+      LONG_NORTE: "-74.04076423783454",
+      LAT_SUR:"4.673272942743481",
+      LONG_SUR: "-74.01566563955751",
+      CD_RGION: this.SessionCDRegion,
+      CD_MNCPIO: this.SessionCDMunicipio
+    }
+    this.sectoresservices.InsertarSector('3', BodyInsert).subscribe(ResultInsert=>{
+      console.log(ResultInsert)
+    })
   }
 
   selectSector(item: any) {
-    console.log(item)
+    this.Cant = '';
+    this.VlrFle = '';
+    this.SectSelec = item.SCTOR_OFRTA;
   }
 
   LimpiaForm() {
