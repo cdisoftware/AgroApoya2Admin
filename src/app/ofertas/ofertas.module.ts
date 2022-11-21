9b48b56a6468105bc661d0a812cc29aa728a7298
edwardfv <edward.ferrucho14@gmail.com>
2022-11-21 14:32:43 -0500

    ajuste

--- src/app/ofertas/ofertas.module.ts
+++ src/app/ofertas/ofertas.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AutocompleteLibModule } from 'angular-ng-autocomplete';
-import { ComprasComponent } from './components/compras/compras.component';
-import { UsuariosComponent } from './components/usuarios/usuarios.component';
+
 
 @NgModule({
   declarations: [
-  
-    ComprasComponent,
-       UsuariosComponent
   ],
   imports: [
     CommonModule,
