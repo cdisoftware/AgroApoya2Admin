364a01e2aadf97a065ffd68ba843cea9cd50e6c3
JuanMoreno1207 <juanbmorenob@hotmail.com>
2023-04-25 10:08:28 -0500

    Ajustes

--- src/app/ofertas/components/admin-ult-milla/admin-ult-milla.component.ts
+++ src/app/ofertas/components/admin-ult-milla/admin-ult-milla.component.ts
@@ -604,6 +604,10 @@ export class AdminUltMillaComponent implements OnInit {
     var color = "";
     for (var i = 0; i < 3; i++) {
       var sub = Math.floor(Math.random() * 256).toString(16);
+      if(sub =='ff'){
+        var sub = Math.floor(Math.random() * 256).toString(16);
+      }
+      
       color += (sub.length == 1 ? "0" + sub : sub);
     }
     return "#" + color;
