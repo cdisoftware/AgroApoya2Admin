f5cd0926ccd1e2da59360d76125891206ffb110f
Juan José Gutiérrez Leguizamón <68406983+JuanGut06@users.noreply.github.com>
2023-12-27 11:42:07 -0500

    ajustes

--- src/app/usuario/components/coordenadasusuarios/coordenadasusuarios.component.ts
+++ src/app/usuario/components/coordenadasusuarios/coordenadasusuarios.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ValorarofertaService } from './../../../core/valoraroferta.service';
-
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-coordenadasusuarios',
   templateUrl: './coordenadasusuarios.component.html',
   styleUrls: ['./coordenadasusuarios.component.css']
 })
 export class CoordenadasusuariosComponent implements OnInit {
+  @ViewChild('ModalRespuesta', { static: false }) ModalRespuesta: any;
 
   ValidaMapSector: string = '0';
 
@@ -19,12 +20,14 @@ export class CoordenadasusuariosComponent implements OnInit {
   Usucodig: string = '0';
   DataUsuarios: any = [];
   VerOcultarCampos: string = '1';
+  Respuesta: string = '';
 
   //Variables Datos usuario
   DatosUser: any = [];
   ValidaInsertSec: string = '1';
 
-  constructor(private serviciosvaloracion: ValorarofertaService,) { }
+  constructor(private serviciosvaloracion: ValorarofertaService,
+    private modalService: NgbModal,) { }
 
   ngOnInit(): void {
     this.ConsultaUsuarios();
@@ -35,7 +38,7 @@ export class CoordenadasusuariosComponent implements OnInit {
     const body = {
       nombres_persona: this.NombreFiltro
     }
-    this.serviciosvaloracion.ConsultaListaPersonas('1', '1', '0', body).subscribe(Resultado => {
+    this.serviciosvaloracion.ConsultaListaPersonas('1', '2', '0', body).subscribe(Resultado => {
       this.ArrayUsucodig = Resultado;
     })
   }
@@ -55,7 +58,7 @@ export class CoordenadasusuariosComponent implements OnInit {
     const body = {
       nombres_persona: this.NombreFiltro
     }
-    this.serviciosvaloracion.ConsultaListaPersonas('1', '0', this.Usucodig, body).subscribe(Resultado => {
+    this.serviciosvaloracion.ConsultaListaPersonas('1', '2', this.Usucodig, body).subscribe(Resultado => {
       this.DataUsuarios = Resultado;
       this.VerOcultarCampos = '2';
     })
@@ -68,14 +71,79 @@ export class CoordenadasusuariosComponent implements OnInit {
   }
 
 
-  AccionMapaUsuario(idValor: any) {  
+  AccionMapaUsuario(idValor: any) {
     this.VerOcultarCampos = '3';
     const body = {
       nombres_persona: idValor.nombres_persona
     }
-    this.serviciosvaloracion.ConsultaListaPersonas('1', '0', idValor.usucodig, body).subscribe(Resultado => {
-      this.DatosUser = Resultado;     
+    this.serviciosvaloracion.ConsultaListaPersonas('1', '2', idValor.usucodig, body).subscribe(Resultado => {
+      this.DatosUser = Resultado;
+      this.Centramapa({ address: Resultado[0].DRCCION + ',' + 'Bogotá' })
     })
   }
 
+  // REGION MAPA
+  geocoder = new google.maps.Geocoder();
+  map: google.maps.Map;
+  markers: google.maps.Marker[] = [];
+  Coor1: string = '';
+  Coor2: string = '';
+  Coordenada: string = '';
+  Centramapa(request: google.maps.GeocoderRequest): void {
+    this.geocoder.geocode(request).then((result) => {
+      const { results } = result;
+      this.map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+          center: results[0].geometry.location,
+          zoom: 15,
+        }
+      );
+      this.Coordenada = results[0].geometry.location.toString().replace('(', '').replace(')', '')
+      const marker = new google.maps.Marker({
+        position: results[0].geometry.location,
+        map: this.map,
+      });
+      this.map.addListener("click", (e: any) => {
+        this.AgregarMarcador(e.latLng, this.map);
+        this.Coordenada = e.latLng.toString().replace('(', '').replace(')', '')
+      });
+      this.markers = [];
+      this.markers.push(marker);
+      this.Coordenada = results[0].geometry.location.toString().replace('(', '').replace(')', '')
+      this.map.setCenter(results[0].geometry.location);
+      return results;
+    })
+      .catch((e) => {
+        console.log("Geocode was not successful for the following reason: " + e);
+      });
+  }
+
+  AgregarMarcador(latLng: google.maps.LatLng, map: google.maps.Map) {
+    if (this.markers.length > 0) {
+      this.markers[0].setMap(null)
+    }
+    const marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+    });
+    this.markers = [];
+    this.markers.push(marker);
+    this.Coordenada = latLng.toString().replace('(', '').replace(')', '')  
+  }
+
+  // FIN REGION MAPA
+  GuardarCoordenada(user: any) {    
+    const body = {
+      Usucodig: user.usucodig,
+      Direccion: "",
+      CompleDirecc: "",
+      Coordenadas: this.Coordenada
+    }
+    this.serviciosvaloracion.ModAdDireccionUser('2', body).subscribe(Resultado => {
+      this.Respuesta = Resultado;
+      this.modalService.open(this.ModalRespuesta, { ariaLabelledBy: 'modal-basic-title', size: 'md' });
+      this.AccionMapaUsuario(user);
+    })
+  }
 }
