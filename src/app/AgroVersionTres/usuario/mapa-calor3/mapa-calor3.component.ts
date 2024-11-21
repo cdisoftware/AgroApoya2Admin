01e1f7b4fcd70bc1e7df15d35c3c814160260a88
LinaFernandaReyes <58578269+DIGITEACHBOX29041209@users.noreply.github.com>
2024-11-21 10:07:58 -0500

    Mapa Calor Corerciones Juan Manuel

--- src/app/AgroVersionTres/usuario/mapa-calor3/mapa-calor3.component.ts
+++ src/app/AgroVersionTres/usuario/mapa-calor3/mapa-calor3.component.ts
@@ -8,68 +8,64 @@ import { ServiciosService } from 'src/app/AgroVersionTres/core/servicios.service
 })
 export class MapaCalor3Component implements OnInit {
 
-  map: google.maps.Map;
-  geocoder = new google.maps.Geocoder();
-  heatmap: google.maps.visualization.HeatmapLayer | undefined;
-
   // Variables para los filtros
-  Comprasfiltros: any = [];
+  ArrayComprasfiltros: any = [];
   ArrayLocalidadesFiltro: any = [];
-  FiltroFechaIncioCompra: string = '';
-  FiltroFechaFinCompra: string = '';
+  FiltroLocalidad: string = '0';
+  FiltroNumeroCompras: string = '0';
   FiltroFechaRegistro: string = '';
-  FiltroLocalidad: string = '';
-  FiltroNumeroCompras: string = '';
+  FiltroFechaFinCompra: string = '';
+  FiltroFechaIncioCompra: string = '';
 
   // Datos para el mapa
-  PinsMapa: any = [];
-  markers: google.maps.Marker[] = [];
+  map: google.maps.Map;
+  geocoder = new google.maps.Geocoder();
+  ArrayMarkers: google.maps.Marker[] = [];
+  AuxMostrarMapa: string = '0';
 
-  constructor(public ServiciosGenerales: ServiciosService) {}
+  constructor(public ServiciosGenerales: ServiciosService) { }
 
   ngOnInit(): void {
     this.CargaIncialListas();
-    this.Centramapa({ address: 'Bogotá, Colombia' });
   }
 
+  //Metodos Generales para el funcionamiento general de la oferta
   CargaIncialListas(): void {
     this.ServiciosGenerales.consTipoLocalidad('2').subscribe(Resultado => {
       this.ArrayLocalidadesFiltro = Resultado;
     });
 
     this.ServiciosGenerales.consMultilistas('1', '0', '0').subscribe(Resultado => {
-      this.Comprasfiltros = Resultado;
+      this.ArrayComprasfiltros = Resultado;
     });
-
   }
-  AgregarMarcador(latLng: google.maps.LatLng, map: google.maps.Map) {
-    const marker = new google.maps.Marker({
-      position: latLng,
-      map: map,
-    });
-    this.markers.push(marker);
-  }
 
+  // Metodos relacionados con los botones de acción de la vista HTML
   BtnBuscar(): void {
+    this.AuxMostrarMapa = '1';
     this.Centramapa({ address: 'Bogotá, Colombia' });
   }
 
   BtnLimpiar(): void {
+    this.AuxMostrarMapa = '0';
     this.FiltroFechaIncioCompra = '';
     this.FiltroFechaFinCompra = '';
     this.FiltroFechaRegistro = '';
-    this.FiltroLocalidad = '';
-    this.FiltroNumeroCompras = '';
-    if (this.heatmap) {
-      this.heatmap.setMap(null);
-    }
-    alert('Filtros limpiados');
+    this.FiltroLocalidad = '0';
+    this.FiltroNumeroCompras = '0';
   }
 
+  BtnDescargarExcel(): void {
+    //TODO FALTA DESCARGAR LA INFORMACION DEL QUE MUESTRA EN EL MAPA EN UN EXCEL
+    alert('Descargar excel')
+  }
+  
+  // Metodos para el funcionamiento de mapa y pintar los pines en el 
   Centramapa(request: google.maps.GeocoderRequest): void {
+    this.ArrayMarkers = [];
     this.geocoder.geocode(request).then((result) => {
       const { results } = result;
-  
+
       // Crea el mapa
       this.map = new google.maps.Map(
         document.getElementById("map") as HTMLElement,
@@ -79,39 +75,38 @@ export class MapaCalor3Component implements OnInit {
           mapTypeId: 'roadmap',
         }
       );
-  
-      // Cuerpo de datos para los filtros
+
+      //TODO VALIDAR LA INFORMACION DE LOS FILTROS PARA ENVIARLA AL MICROSERVICIO
       const body = {
         FechaIncioCompra: this.FiltroFechaIncioCompra || "2023-05-02",
         FechaFinCompra: "2023-05-02",
         FechaRegistro: "2023-05-02",
       };
-  
-      // Llamada al servicio para obtener las coordenadas
-      this.ServiciosGenerales.consMapaCalor('1', '0', '0', body).subscribe((Rest) => {
-        console.log(Rest);
-  
-        const features = [];
 
+      this.ServiciosGenerales.consMapaCalor('1', '0', '0', body).subscribe((Rest) => {
+        const features = [];
         for (let i = 0; i < Rest.length; i++) {
-          console.log(Rest[i].Coordenadas)
           var auxsplit = Rest[i].Coordenadas.split(',');
           var lat = parseFloat(auxsplit[0]);
           var long = parseFloat(auxsplit[1]);
           features.push({ position: new google.maps.LatLng(lat, long) });
         }
-
         for (let i = 0; i < features.length; i++) {
           this.AgregarMarcador(features[i].position, this.map);
         }
-        alert(features.length)
-
-
       });
-
       return results;
     }).catch((e) => {
       console.log("Geocode was not successful for the following reason: " + e);
     });
   }
+
+  AgregarMarcador(latLng: google.maps.LatLng, map: google.maps.Map) {
+    const marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+    });
+    this.ArrayMarkers.push(marker);
+  }
+
 }
