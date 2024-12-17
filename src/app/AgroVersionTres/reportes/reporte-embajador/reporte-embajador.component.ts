78553374b64c989ca82c432d0c35b3618f725325
LinaFernandaReyes <58578269+DIGITEACHBOX29041209@users.noreply.github.com>
2024-12-17 10:13:27 -0500

    Ajustes

--- src/app/AgroVersionTres/reportes/reporte-embajador/reporte-embajador.component.ts
+++ src/app/AgroVersionTres/reportes/reporte-embajador/reporte-embajador.component.ts
@@ -1,28 +1,22 @@
-
-import { Component, OnInit } from '@angular/core';
-import { ServiciosService } from 'src/app/AgroVersionTres/core/servicios.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ViewChild } from '@angular/core';
-
 import * as fs from 'file-saver';
 import { Workbook } from 'exceljs'
-
+import { ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ServiciosService } from 'src/app/AgroVersionTres/core/servicios.service';
 
 @Component({
   selector: 'app-reporte-embajador',
   templateUrl: './reporte-embajador.component.html',
   styleUrls: ['./reporte-embajador.component.css']
 })
-export class ReporteEmbajadorComponent implements OnInit {
-[x: string]: any;
 
- 
+export class ReporteEmbajadorComponent implements OnInit {
+
   mostrarTabla: string = '0'; //por defecto esta oculta , 1--> Muestra, 0--> Oculta
   ListaLocalidad: any = [];
   ListaTabla: any = [];
-  ReporteVecinos: any = [];
-  MostrarTablaModal : string = '0';
-
+  ListaVecinos: any = [];
 
   //VARIABLES DE LOS FILTROS
   FechaInicioEmba: string = '';
@@ -33,7 +27,7 @@ export class ReporteEmbajadorComponent implements OnInit {
   CodigoUsuarioVecino: string = '';
   CorreoVeci: string = '';
   TelefonoVeci: string = '';
-  localidad: any = [];
+  localidad: string = '0';
 
   //VARIABLES DEL MODAL VECINOS
   Idusuario: any;
@@ -67,43 +61,66 @@ export class ReporteEmbajadorComponent implements OnInit {
   }
 
   //Metodos accion botones filtros
-  btnBuscar() {
-    // CREAR VARIABLES LOCALES 
-    var codigo: string = '0';
-    var auxCorreo: string= '0';
-
-    if(this.CodigoUsuarioVecino != '' && this.CodigoUsuarioVecino != undefined && this.CodigoUsuarioVecino != null ){
-      codigo = this.CodigoUsuarioVecino;
-    }
-    if(this.Correo != '' && this.Correo != undefined && this.Correo != null ){
-      auxCorreo = this.Correo;
-    }
+  BtnBuscar() {
     this.mostrarTabla = '1';
-    const body = {
-      Fechainicio: "0",
-      Fechafin: "0"
+
+    const formatearFecha = (fecha: string): string => {
+      if (fecha && fecha.includes('-')) {
+        const partes = fecha.split('-'); // Divide la fecha en [año, mes, día]
+        return partes.join('/'); // Une las partes con '/'
+      }
+      return '0'; // Devuelve '0' si la fecha es inválida
     };
-    this.ServiciosGenerales.consEmbajadorConjuntosReporte('1', codigo, auxCorreo, '0', '0', '0', '0', body)
-    .subscribe(Rest => {
-      console.log('Respuesta del Servicio:', Rest); 
-      this.ListaTabla = Rest;
-    }, error => {
-      console.error('Error en el servicio:', error);
-    });
-  
+
+    // Método auxiliar para verificar y asignar valores
+    const validarValor = (valor: any): string => (valor && valor !== '' ? valor : '0');
+
+    // Uso del método `formatearFecha` para las fechas
+    var auxFechaIncio = formatearFecha(this.FechaInicioEmba);
+    var auxFechaFin = formatearFecha(this.FechaFinEmba);
+
+    // Asignación de valores con el método auxiliar
+    auxFechaIncio = validarValor(auxFechaIncio);
+    auxFechaFin = validarValor(auxFechaFin);
+    const auxCodigoEmbajador = validarValor(this.CodigoUsuarioEmba);
+    const auxCorreoEmbajador = validarValor(this.CorreoEmba);
+    const auxTelefonoEmbajador = validarValor(this.telefonoEmbajador);
+    const auxCodigoVecino = validarValor(this.CodigoUsuarioVecino);
+    const auxCorreoVecino = validarValor(this.CorreoVeci);
+    const auxTelefonoVecino = validarValor(this.TelefonoVeci);
+    const auxLocalidad = validarValor(this.localidad);
+
+    const body = {
+      Fechainicio: auxFechaIncio,
+      Fechafin: auxFechaFin,
+    };
+
+    this.ServiciosGenerales.consEmbajadorConjuntosReporte(
+      '1', auxCodigoEmbajador, auxCorreoEmbajador, auxTelefonoEmbajador, auxCodigoVecino, auxCorreoVecino, auxTelefonoVecino, body
+    ).subscribe(
+      Rest => {
+        console.log('Respuesta del Servicio:', Rest);
+        this.ListaTabla = Rest;
+      },
+      error => {
+        console.error('Error en el servicio:', error);
+      }
+    );
   }
+
   BtnLimpiar(): void {
     this.FechaInicioEmba = '';
-    this.FechaFinEmba ='';
-    this.CodigoUsuarioEmba= '';
-    this.CorreoEmba= '';
-    this.telefonoEmbajador= '';
-    this.CodigoUsuarioVecino= '';
+    this.FechaFinEmba = '';
+    this.CodigoUsuarioEmba = '';
+    this.CorreoEmba = '';
+    this.telefonoEmbajador = '';
+    this.CodigoUsuarioVecino = '';
     this.CorreoVeci = '';
-    this.TelefonoVeci ='';
-    this.localidad ='0';
+    this.TelefonoVeci = '';
+    this.localidad = '0';
     this.mostrarTabla = '0';
   }
+
   AbrirModal(ListaTabla: any): void {
     this.modalService.open(this.ModalVecino, { ariaLabelledBy: 'modal-basic-title', size: 'xl' })
 
@@ -119,6 +136,7 @@ export class ReporteEmbajadorComponent implements OnInit {
     this.Linkrelacionvecinos = ListaTabla.LinkCortoVecino
     this.Numerovecinos = ListaTabla.NumeroVecino
     this.Fechaincioembajador = ListaTabla.FechaCreacioncomoEmbajador
+    
     console.log(ListaTabla)
 
     this.VecinosListas(ListaTabla.IdUsuarioEmbajador);
@@ -126,12 +144,12 @@ export class ReporteEmbajadorComponent implements OnInit {
 
   VecinosListas(UsucodigEmbajador: string): void {
     this.ServiciosGenerales.consEmbajadorVecinosReporte('1', UsucodigEmbajador).subscribe(Rest => {
-      this.ReporteVecinos = Rest;
+      this.ListaVecinos = Rest;
       console.log(Rest);
 
-      if(this.ReporteVecinos.length == 0){
+      if (this.ListaVecinos.length == 0) {
         this.ExelVecino = '0';
-      }else{
+      } else {
         this.ExelVecino = '1';
       }
     },
@@ -140,57 +158,58 @@ export class ReporteEmbajadorComponent implements OnInit {
       });
   }
 
-  DescargarExcelEmbajadorVecinos(){
-        //Crear nuevo archiv
-        let workbook = new Workbook();
-  
-        // crear una nueva hoja dentro del excel
-        let worksheet = workbook.addWorksheet("Reporte Vecinos"); //Nombre de la hoja
-        let header = ['IdUsuarioEmbajador','Nombre','Correo','FechaCreacion','Celular','id_manychat','DRCCION',
-          'CMPLMNTO_DRRCCION','id']; //Encabezado y se dejan las columnas que corresponden 
-  
-  
-        worksheet.addRow(header); //le da los estilos a  header de la tabla
-        ['A1', 'B1', 'C1', 'D1', 'E1', 'G1', 'F1', 'H1', 'I1'].map(key => {
-          worksheet.getCell(key).fill = {
-            type: 'pattern',
-            pattern: 'darkTrellis',
-            fgColor: { argb: '397c97' },
-            bgColor: { argb: '397c97' }
-          };
-          worksheet.getCell(key).font = {
-            color: { argb: 'FFFFFF' }
-          };
-        });
-  
-        worksheet.columns = [ // le dan el tamaño a las columnas
-          { width: 25 }, { width: 30 }, { width: 30 }, { width: 20 }, { width: 20 },
-        { width: 20 }, { width: 25 }, { width: 30 }, { width: 30 }, { width: 20 },
-        { width: 15 }, { width: 25 }
-        ];  
-        //PARTE DINAMINCA!!
-        //Recorre el arreglo que quiero poner en el excel
-        for (let x1 of this.ReporteVecinos) {
-          let temp = []
-          // EN EL ORDERN QUE LOS PONGAS ACA SE VAN A VER EN LA HOJA DEL EXCEL
-          
-          temp.push(x1['UsucodigEmbajador'])
-          temp.push(x1['NombreVecino'])
-          temp.push(x1['CorreoVecino'])
-          temp.push(x1['FechaCreacionVecino'])
-          temp.push(x1['CelularVecino'])
-          temp.push(x1['IdManychatVecino'])
-          temp.push(x1['DireccionVecino'])
-          temp.push(x1['ComplementoVecino'])
-          temp.push(x1['Id'])
-          
-          worksheet.addRow(temp)
-        }
-  
-        let fname = "Reporte-Embajador";
-        workbook.xlsx.writeBuffer().then((data) => {
-          let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-          fs.saveAs(blob, fname + '.xlsx');
-        });
-      } 
+  DescargarExcelEmbajadorVecinos() {
+    //Crear nuevo archiv
+    let workbook = new Workbook();
+
+    // crear una nueva hoja dentro del excel
+    let worksheet = workbook.addWorksheet("Reporte Vecinos"); //Nombre de la hoja
+    let header = ['IdUsuarioEmbajador', 'Nombre', 'Correo', 'FechaCreacion', 'Celular', 'id_manychat', 'DRCCION',
+      'CMPLMNTO_DRRCCION', 'id']; //Encabezado y se dejan las columnas que corresponden 
+
+
+    worksheet.addRow(header); //le da los estilos a  header de la tabla
+    ['A1', 'B1', 'C1', 'D1', 'E1', 'G1', 'F1', 'H1', 'I1'].map(key => {
+      worksheet.getCell(key).fill = {
+        type: 'pattern',
+        pattern: 'darkTrellis',
+        fgColor: { argb: '397c97' },
+        bgColor: { argb: '397c97' }
+      };
+      worksheet.getCell(key).font = {
+        color: { argb: 'FFFFFF' }
+      };
+    });
+
+    worksheet.columns = [ // le dan el tamaño a las columnas
+      { width: 25 }, { width: 30 }, { width: 30 }, { width: 20 }, { width: 20 },
+      { width: 20 }, { width: 25 }, { width: 30 }, { width: 30 }, { width: 20 },
+      { width: 15 }, { width: 25 }
+    ];
+    //PARTE DINAMINCA!!
+    //Recorre el arreglo que quiero poner en el excel
+    for (let x1 of this.ListaVecinos) {
+      let temp = []
+      // EN EL ORDERN QUE LOS PONGAS ACA SE VAN A VER EN LA HOJA DEL EXCEL
+
+      temp.push(x1['UsucodigEmbajador'])
+      temp.push(x1['NombreVecino'])
+      temp.push(x1['CorreoVecino'])
+      temp.push(x1['FechaCreacionVecino'])
+      temp.push(x1['CelularVecino'])
+      temp.push(x1['IdManychatVecino'])
+      temp.push(x1['DireccionVecino'])
+      temp.push(x1['ComplementoVecino'])
+      temp.push(x1['Id'])
+
+      worksheet.addRow(temp)
     }
+
+    let fname = "Reporte-Embajador";
+    workbook.xlsx.writeBuffer().then((data) => {
+      let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      fs.saveAs(blob, fname + '.xlsx');
+    });
+  }
+
+}
